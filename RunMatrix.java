99c17230223864d8c666679f1ff3e08b6adff333
maizelstrang <mastrang@hartford.edu>
2022-11-15 15:34:56 -0500

    Update RunMatrix.java
    Ask for user input

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
 public class RunMatrix {
+    public static void main(String[] args){
 
+        System.out.print("Please enter the size of your matrix:");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+    }
 }
