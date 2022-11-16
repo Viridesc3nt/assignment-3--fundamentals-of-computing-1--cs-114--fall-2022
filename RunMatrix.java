54c1b496a9750033b5733ac31e22e3ba2564007e
maizelstrang <mastrang@hartford.edu>
2022-11-16 08:52:56 -0500

    Populate matrix starting at 0

--- RunMatrix.java
+++ RunMatrix.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.print("Please enter the size of your matrix:");
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        System.out.print("Please enter the size of your matrix: ");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
 
+            int[][] table = new int[input][input];
+
+        // Load the table with values
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                table[row][col] = (row * input) + col;
+            }
+        }
+
+
+        // Print the table
+        for (int row = 0; row < table.length; row++) {
+            for (int col = 0; col < table[row].length; col++) {
+                System.out.print (table[row][col] + "\t");
+            }
+
+                System.out.println();
+        }
     }
 }
